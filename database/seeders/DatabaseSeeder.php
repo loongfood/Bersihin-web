157328d13e38c13f5b30f579213b306256034f13
Angga pajri padilah <anggapajripdlh10@gmail.com>
2026-07-14 03:47:40 +0700

    implement edit, delete, dan seeder untuk laporan dengan penanganan foto

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -20,6 +20,7 @@ class DatabaseSeeder extends Seeder
             UserSeeder::class,
             KategoriSampahSeeder::class,
             JadwalPengangkutanSeeder::class,
+            LaporanSeeder::class,
         ]);
     }
 }
